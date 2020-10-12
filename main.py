43503b7c557d3cf44647430561b3348df223e1e3
Xiyue Li <lxy981126@outlook.com>
2020-10-12 18:56:17 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from sklearn.naive_bayes import GaussianNB
+
+
+def gnb():
+
+    print("this is Gaussian naive bayes model")
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    gnb()
+
