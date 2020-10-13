0a40670ab283a914847745bddb22de990aa4441e
Xiyue Li <lxy981126@outlook.com>
2020-10-13 18:46:26 +0800

    adjust model parameters

--- BaseDT.py
+++ BaseDT.py
@@ -7,7 +7,7 @@ def base_decision_tree(training, test, output):
     training_data = np.loadtxt(training, delimiter=',')
     test_data = np.loadtxt(test, delimiter=',')
 
-    dt = DecisionTreeClassifier()
+    dt = DecisionTreeClassifier(criterion='entropy')
     dt.fit(training_data[:, :-1], training_data[:, -1])
     test_prediction = dt.predict(test_data[:, :-1])
     helper.output_computed_metrics(test_data, test_prediction, output)
@@ -16,5 +16,5 @@ def base_decision_tree(training, test, output):
 if __name__ == '__main__':
     training_path = input('Enter path of training data')#"./Assig1-Dataset/train_2.csv"
     test_path = input('Enter path of test data')#"./Assig1-Dataset/test_with_label_2.csv"
-    output_path = input('Enter path of output csv')#"./Assig1-Dataset/Base-DT-DS2.csv"
+    output_path = input('Enter path of output csv')#"./Output/Base-DT-DS2.csv"
     base_decision_tree(training_path, test_path, output_path)
--- BaseMLP.py
+++ BaseMLP.py
@@ -7,7 +7,7 @@ def base_mlp(training, test, output):
     training_data = np.loadtxt(training, delimiter=',')
     test_data = np.loadtxt(test, delimiter=',')
 
-    mlp = MLPClassifier()
+    mlp = MLPClassifier(hidden_layer_sizes=(1, 100), activation='logistic', solver='sgd')
     mlp.fit(training_data[:, :-1], training_data[:, -1])
     test_prediction = mlp.predict(test_data[:, :-1])
 
@@ -15,7 +15,7 @@ def base_mlp(training, test, output):
 
 
 if __name__ == '__main__':
-    training_path = "./Assig1-Dataset/train_2.csv"#input('Enter path of training data')
-    test_path = "./Assig1-Dataset/test_with_label_2.csv"#input('Enter path of test data')
-    output_path = "./Output/Base-MLP-DS2.csv"#input('Enter path of output csv')
+    training_path = input('Enter path of training data')  # "./Assig1-Dataset/train_2.csv"
+    test_path = input('Enter path of test data')  # "./Assig1-Dataset/test_with_label_2.csv"
+    output_path = input('Enter path of output csv')  # "./Output/Base-MLP-DS2.csv"
     base_mlp(training_path, test_path, output_path)
