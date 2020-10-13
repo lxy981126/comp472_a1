b2dc399107c44cdf03bcfdfec1b60a581bbeb9f5
Xiyue Li <lxy981126@outlook.com>
2020-10-13 18:05:28 +0800

    Perceptron, move output files

--- BaseDT.py
+++ BaseDT.py
@@ -14,7 +14,7 @@ def base_decision_tree(training, test, output):
 
 
 if __name__ == '__main__':
-    training_path = "./Assig1-Dataset/train_2.csv"#input('Enter path of training data')
-    test_path = "./Assig1-Dataset/test_with_label_2.csv"#input('Enter path of test data')
-    output_path = "./Assig1-Dataset/Base-DT-DS2.csv"#input('Enter path of output csv')
+    training_path = input('Enter path of training data')#"./Assig1-Dataset/train_2.csv"
+    test_path = input('Enter path of test data')#"./Assig1-Dataset/test_with_label_2.csv"
+    output_path = input('Enter path of output csv')#"./Assig1-Dataset/Base-DT-DS2.csv"
     base_decision_tree(training_path, test_path, output_path)
